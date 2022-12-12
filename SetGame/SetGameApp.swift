b4bc36aa2a9b6b1d6388c5b9bc31fc72c29d98c7
Ben Craig <benjaminrichardcraig@gmail.com>
2022-12-12 17:10:24 -0600

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Benjamin Craig on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
